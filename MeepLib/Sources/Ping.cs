b080516c2808d9a1e5bdc340ef1eb74caf919d23
Chris Wenham <cwenham@miomni.com>
2018-06-16 22:06:35 +0100

    Add Ping and Random modules.

---
+++ MeepLib/Sources/Ping.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+using SmartFormat;
+
+using MeepModel.Messages;
+
+namespace MeepLib.Sources
+{
+    public class Ping : AMessageModule
+    {
+        /// <summary>
+        /// Address of host to ping, in {Smart.Format}
+        /// </summary>
+        /// <value>To.</value>
+        public string To { get; set; }
+
+        /// <summary>
+        /// Padding added to response time, for hosts less than 1ms away (same machine or rack)
+        /// </summary>
+        /// <value>The padding.</value>
+        /// <remarks>The main use is to distinguish between no-response and a sub-milisecond
+        /// response on health monitoring systems that ignore error messages and just display 
+        /// a number, like a packed Telemetry dashboard.</remarks>
+        public TimeSpan Padding { get; set; } = TimeSpan.Zero;
+
+        private System.Net.NetworkInformation.Ping Pinger { get; set; }
+
+        public override async Task<Message> HandleMessage(Message msg)
+        {
+            MessageContext context = new MessageContext(msg, this);
+            string pingTo = Smart.Format(To, context);
+
+            if (Pinger == null)
+                Pinger = new System.Net.NetworkInformation.Ping();
+
+            var response = await Pinger.SendPingAsync(pingTo, (int)Timeout.TotalMilliseconds);
+
+            if (response.Status == IPStatus.Success)
+                return new Step
+                {
+                    DerivedFrom = msg,
+                    Number = ((Step)msg)?.Number ?? 0,
+                    Value = response.RoundtripTime + Padding.TotalMilliseconds
+                };
+            else
+                return new Message
+                {
+                    DerivedFrom = msg,
+                    Value = response.Status.ToString()
+                };
+        }
+    }
+}
--- MeepLib/Sources/Random.cs
+++ MeepLib/Sources/Random.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
+
+using MeepLib.MeepLang;
+using MeepModel.Messages;
+
 namespace MeepLib.Sources
 {
-    public class Random
+    /// <summary>
+    /// Generate random numbers
+    /// </summary>
+    public class Random : AMessageModule
     {
-        public Random()
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+
+        private System.Random _rand = new System.Random();
+
+        public override async Task<Message> HandleMessage(Message msg)
         {
+            return await Task.Run<Message>(() =>
+            {
+                if (Min != 0 || Max != 0)
+                    return new Message
+                    {
+                        DerivedFrom = msg,
+                        Value = _rand.Next(Min, Max)
+                    };
+                else
+                    return new Message
+                    {
+                        DerivedFrom = msg,
+                        Value = _rand.NextDouble()
+                    };
+            });
         }
     }
 }
