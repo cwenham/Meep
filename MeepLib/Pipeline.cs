1ffc562e2174f561d32d39c40ffac7d600553fa6
Chris Wenham <cwenham@miomni.com>
2018-07-08 18:45:26 +0100

    Add needed XmlRoot attribute. Now passes Deserialise unit test.

--- MeepLib/Pipeline.cs
+++ MeepLib/Pipeline.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Xml.Serialization;
 
 namespace MeepLib
 {
+    [XmlRoot(ElementName = "Pipeline", Namespace = "http://meep.example.com/Meep/V1")]
     public class Pipeline : AMessageModule
     {
     }
