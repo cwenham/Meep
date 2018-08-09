8bda1d66bf3d7d9a6db66f458b927d2aa891fdf1
Chris Wenham <cwenham@miomni.com>
2018-08-09 20:28:25 +0100

    Net-Core 2.1
    Add simple Console.WriteLine module.

---
+++ MeepLib/Outputs/WriteLine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using SmartFormat;
+
+using MeepLib.Messages;
+
+namespace MeepLib.Outputs
+{
+    /// <summary>
+    /// Write to the console
+    /// </summary>
+    public class WriteLine : AMessageModule
+    {
+        /// <summary>
+        /// String data to write, in {Smart.Format}
+        /// </summary>
+        /// <value></value>
+        /// <remarks>Defaults to message serialised as JSON.</remarks>
+        public string From { get; set; } = "{msg.AsJSON}";
+
+        public override async Task<Message> HandleMessage(Message msg)
+        {
+            return await Task.Run<Message>(() =>
+            {
+                MessageContext context = new MessageContext(msg, this);
+                string line = Smart.Format(From, context);
+
+                Console.WriteLine(line);
+
+                return msg;
+            });
+        }
+    }
+}
