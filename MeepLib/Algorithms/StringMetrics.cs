d0c4689f8bf2c132b276a09d219e33ec287d1259
cwenham <cwenham@gmail.com>
2018-12-07 21:49:48 +0000

    Plugin functions for NCalc expressions in Where.

---
+++ MeepLib/Algorithms/StringMetrics.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NCalc;
+
+namespace MeepLib.Algorithms
+{
+    /// <summary>
+    /// Functions for calculating values derived from analysis of strings/text
+    /// </summary>
+    public static class StringMetrics
+    {
+        /// <summary>
+        /// Returns bits of entropy represented in a given string, per 
+        /// http://en.wikipedia.org/wiki/Entropy_(information_theory) 
+        /// </summary>
+        /// <remarks>From Jeff Atwood's self-answer at:
+        /// https://codereview.stackexchange.com/questions/868/calculating-entropy-of-a-string</remarks>
+        public static double ShannonEntropy(string s)
+        {
+            var map = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (!map.ContainsKey(c))
+                    map.Add(c, 1);
+                else
+                    map[c] += 1;
+            }
+
+            double result = 0.0;
+            int len = s.Length;
+            foreach (var item in map)
+            {
+                var frequency = (double)item.Value / len;
+                result -= frequency * (Math.Log(frequency) / Math.Log(2));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Version to be used in NCalc expressions
+        /// </summary>
+        /// <returns>The entropy.</returns>
+        /// <param name="args">Arguments.</param>
+        public static object ShannonEntropy(FunctionArgs args)
+        {
+            string input = (string)args.Parameters[0].Evaluate();
+
+            if (!(input is null))
+                return ShannonEntropy(input);
+
+            return 0.0;
+        }
+
+        /// <summary>
+        /// Length of string, for use in NCalc expressions
+        /// </summary>
+        /// <returns>The length.</returns>
+        /// <param name="args">Arguments.</param>
+        public static object Length(FunctionArgs args)
+        {
+            string input = (string)args.Parameters[0].Evaluate();
+
+            if (!(input is null))
+                return input.Length;
+
+            return 0;
+        }
+    }
+}
--- MeepLib/Filters/Where.cs
+++ MeepLib/Filters/Where.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using sle = System.Linq.Expressions;
 
 using NCalc;
 using SmartFormat;
@@ -16,6 +20,50 @@ namespace MeepLib.Filters
     [Macro(DefaultProperty = "Expr", Name = "Where", Position = MacroPosition.Upstream)]
     public class Where : AFilter
     {
+        public Where()
+        {
+            // Search for classes that implement compatible functions. If a plugin
+            // has been loaded before this point, it'll discover functions exposed
+            // from there. Must be a static class with functions that accept an
+            // instance of FunctionArgs as their only parameter.
+
+            if (_functions == null)
+            {
+                var staticClasses = from a in AppDomain.CurrentDomain.GetAssemblies()
+                                    from t in a.GetTypes()
+                                    where t.IsAbstract && t.IsSealed
+                                    select t;
+
+                var staticMethods = from t in staticClasses
+                                    from f in t.GetMethods(BindingFlags.Static | BindingFlags.Public)
+                                    select f;
+
+                var matchingFuncs = from f in staticMethods
+                                    let fParams = f.GetParameters()
+                                    where fParams.Any()
+                                    && fParams.Count() == 1
+                                    && fParams.First().ParameterType == typeof(FunctionArgs)
+                                    select f;
+
+                _functions = matchingFuncs.ToDictionary(x => x.Name, ToFunc);
+            }
+        }
+
+        /// <summary>
+        /// Convert MethodInfo into a Func
+        /// </summary>
+        /// <returns>The func.</returns>
+        /// <param name="method">MethodInfo</param>
+        /// <remarks>Calling a delegate is significantly faster than using
+        /// MethodInfo.Invoke, and typically only about 20% slower than a direct
+        /// call.</remarks>
+        private Func<FunctionArgs, object> ToFunc(MethodInfo method)
+        {
+            return (Func<FunctionArgs, object>)Delegate.CreateDelegate(typeof(Func<FunctionArgs, object>), method);
+        }
+
+        private static Dictionary<string, Func<FunctionArgs, object>> _functions;
+
         /// <summary>
         /// Boolean expression in {Smart.Format} and NCalc format
         /// </summary>
@@ -55,6 +103,7 @@ namespace MeepLib.Filters
                     {
                         parameterised = Expr.ToSmartParameterised("[arg{0}]");
                         expression = new Expression(parameterised[0]);
+                        expression.EvaluateFunction += Expression_EvaluateFunction;
                         // _lambda = expression.ToLambda<Message, Boolean>();
                     }
 
@@ -74,6 +123,12 @@ namespace MeepLib.Filters
                     return null;
                 }
             });
+        }
+
+        void Expression_EvaluateFunction(string name, FunctionArgs args)
+        {
+            if (_functions.ContainsKey(name))
+                args.Result = _functions[name](args);
         }
     }
 }
