5f50cb471afd23b0ab05f50d90ef64df858ddf2b
Chris Wenham <cwenham@gmail.com>
2019-04-28 12:51:36 +0100

    Remove dupe "using"

--- MeepModel/Messages/RecordMessage.cs
+++ MeepModel/Messages/RecordMessage.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Xml.Serialization;
 using System.ComponentModel.DataAnnotations;
