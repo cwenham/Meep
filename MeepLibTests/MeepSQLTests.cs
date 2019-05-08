92bb869473e3d8304fa145e3066f595aa655dae9
Chris Wenham <cwenham@gmail.com>
2019-05-08 15:52:08 +0100

    Fix CreateTableSQL test (Win vs. nix line endings, plus missing unique index on PK).

--- MeepLibTests/MeepSQLTests.cs
+++ MeepLibTests/MeepSQLTests.cs
@@ -3,6 +3,7 @@
 using Xunit;
 
 using MeepSQL;
+using MeepLib;
 using MeepLib.Messages;
 
 namespace MeepLibTests
@@ -18,7 +19,7 @@ namespace MeepLibTests
 
             string create1 = msg.ToTableDef("Messages").ToCreateTable();
 
-            Assert.Equal(CreateTableSQL1, create1);
+            Assert.Equal(CreateTableSQL1.ToUnixEndings(), create1.ToUnixEndings());
         }
 
         public static string CreateTableSQL1 = @"CREATE TABLE IF NOT EXISTS Messages (
@@ -26,6 +27,7 @@ ID varchar(36) NOT NULL PRIMARY KEY,
 DerivedFromID varchar(36) NOT NULL,
 CreatedTicks bigint NOT NULL
 );
+CREATE UNIQUE INDEX IF NOT EXISTS Messages_ID_idx ON Messages (ID);
 CREATE INDEX IF NOT EXISTS Messages_DerivedFromID_idx ON Messages (DerivedFromID);
 CREATE INDEX IF NOT EXISTS Messages_CreatedTicks_idx ON Messages (CreatedTicks);
 ";
