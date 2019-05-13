284bacb14fbf9214e9803f9362052d1c5369250e
cwenham <cwenham@gmail.com>
2019-05-13 20:39:26 +0100

    NCalc date and time functions.

---
+++ MeepLib/Algorithms/DateTimeFuncs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NCalc;
+
+namespace MeepLib.Algorithms
+{
+    /// <summary>
+    /// Static functions for NCalc expressions
+    /// </summary>
+    public static class DateTimeFuncs
+    {
+        /// <summary>
+        /// How long since a given time
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static object UtcAge(FunctionArgs args)
+        {
+            DateTime input = (DateTime)args.Parameters[0].Evaluate();
+            if (input != null)
+                return DateTime.UtcNow - input;
+
+            return TimeSpan.Zero;
+        }
+
+        public static object InDays(FunctionArgs args)
+        {
+            TimeSpan input = (TimeSpan)args.Parameters[0].Evaluate();
+            if (input != null)
+                return input.TotalDays;
+
+            return 0;
+        }
+
+        public static object InHours(FunctionArgs args)
+        {
+            TimeSpan input = (TimeSpan)args.Parameters[0].Evaluate();
+            if (input != null)
+                return input.TotalHours;
+
+            return 0;
+        }
+
+        public static object InSeconds(FunctionArgs args)
+        {
+            TimeSpan input = (TimeSpan)args.Parameters[0].Evaluate();
+            if (input != null)
+                return input.TotalSeconds;
+
+            return 0;
+        }
+
+        public static object InMilliseconds(FunctionArgs args)
+        {
+            TimeSpan input = (TimeSpan)args.Parameters[0].Evaluate();
+            if (input != null)
+                return input.TotalMilliseconds;
+
+            return 0;
+        }
+    }
+}
