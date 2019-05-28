99780efa65f0c3cad9655c4525c7945f640efd80
Chris Wenham <cwenham@gmail.com>
2019-05-29 00:21:32 +0100

    Support automatically fetching and unzipping plugins if given a URL.

--- MeepLib/MeepLang/XPluginReader.cs
+++ MeepLib/MeepLang/XPluginReader.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using System.IO.Compression;
 using System.Reflection;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 using SmartFormat;
 using NLog;
@@ -29,13 +33,13 @@ namespace MeepLib.MeepLang
         /// <summary>
         /// Queue of possible paths to the same plugin, populated by the primary element and &lt;Fallback&gt;s
         /// </summary>
-        private Queue<string> PossiblePaths;
+        private Queue<string> PossibleURLs;
 
         public override bool Read()
         {
             bool read = base.Read();
 
-            string filename = null;
+            string url = null;
 
             // Populate the queue with primary filenames and Fallback paths
             if (read && _Reader.NodeType == XmlNodeType.Element)
@@ -43,17 +47,17 @@ namespace MeepLib.MeepLang
                     switch (_Reader.LocalName)
                     {
                         case "Plugin":
-                            PossiblePaths = new Queue<string>();
-                            filename = GetAttribute("File");
-                            if (!String.IsNullOrWhiteSpace(filename))
-                                PossiblePaths.Enqueue(filename);
+                            PossibleURLs = new Queue<string>();
+                            url = GetAttribute("Url") ?? GetAttribute("File");
+                            if (!String.IsNullOrWhiteSpace(url))
+                                PossibleURLs.Enqueue(url);
                             break;
                         case "Fallback":
-                            if (PossiblePaths != null)
+                            if (PossibleURLs != null)
                             {
-                                filename = GetAttribute("File");
-                                if (!String.IsNullOrWhiteSpace(filename))
-                                    PossiblePaths.Enqueue(filename);
+                                url = GetAttribute("Url") ?? GetAttribute("File");
+                                if (!String.IsNullOrWhiteSpace(url))
+                                    PossibleURLs.Enqueue(url);
                             }
                             break;
                         default:
@@ -61,29 +65,18 @@ namespace MeepLib.MeepLang
                     }
 
             // Go through the queue and try to load the plugin once we reach a </Plugin> or <Plugin .../>
-            if (read && _Reader.LocalName == "Plugin" && PossiblePaths != null && (_Reader.NodeType == XmlNodeType.EndElement || _Reader.IsEmptyElement))
+            if (read && _Reader.LocalName == "Plugin" && PossibleURLs != null && (_Reader.NodeType == XmlNodeType.EndElement || _Reader.IsEmptyElement))
             {
                 bool loaded = false;
-                while (!loaded && PossiblePaths.Count > 0)
+                while (!loaded && PossibleURLs.Count > 0)
                     try
                     {
-                        filename = PossiblePaths.Dequeue();
-                        if (filename != null)
+                        url = PossibleURLs.Dequeue();
+                        if (url != null)
                         {
-                            string filepath = null;
-                            if (File.Exists(filename))
-                                filepath = filename;
-                            else
-                                filepath = Path.Combine(pluginDir, filename);
-
-                            if (File.Exists(filepath))
-                            {
-                                Assembly.LoadFrom(filepath);
-                                XMeeplangDeserialiser.InvalidateCache();
-                                loaded = true;
-                            }
-                            else
-                                logger.Warn($"Plugin {filename} not found");
+                            var task = LoadPlugin(url);
+                            task.Wait();
+                            loaded = task.Result;
                         }
                     }
                     catch (Exception ex)
@@ -91,10 +84,116 @@ namespace MeepLib.MeepLang
                         logger.Warn(ex, $"{ex.GetType().Name} thrown when loading plugin: {ex.Message}");
                     }
 
-                PossiblePaths = null;
+                if (!loaded)
+                    logger.Error("Could not find plugin {0}", url);
+
+                PossibleURLs = null;
             }
 
             return read;
+        }
+
+        public async Task<bool> LoadPlugin(string pluginUrl)
+        {
+            try
+            {
+                string filepath = null;
+
+                if (File.Exists(pluginUrl))
+                    filepath = pluginUrl;
+                else
+                {
+                    Uri uri = new Uri(pluginUrl);
+                    filepath = FindLocalisedPlugin(uri);
+
+                    if (filepath is null)
+                        using (HttpClient client = new HttpClient())
+                        {
+                            var streamTask = client.GetStreamAsync(uri);
+                            string localDir = LocalDirectory(uri);
+                            if (!Directory.Exists(localDir))
+                                Directory.CreateDirectory(localDir);
+
+                            var stream = await streamTask;
+                            string localisedFile = Path.Combine(localDir, Path.GetFileName(pluginUrl));
+                            using (FileStream fstream = new FileStream(localisedFile, FileMode.CreateNew))
+                            {
+                                await stream.CopyToAsync(fstream);
+                            }
+
+                            if (Path.GetExtension(pluginUrl).Equals(".zip", StringComparison.OrdinalIgnoreCase))
+                            {
+                                ZipFile.ExtractToDirectory(localisedFile, localDir);
+                            }
+                        }
+
+                    filepath = FindLocalisedPlugin(uri);
+                }
+
+                if (filepath != null && File.Exists(filepath))
+                {
+                    Assembly.LoadFrom(filepath);
+                    XMeeplangDeserialiser.InvalidateCache();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "{0} thrown trying to load plugin {1}: {2}", ex.GetType().Name, pluginUrl, ex.Message);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Searches for already existing local plugin
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns>Path to DLL, or null if it doesn't appear to be localised yet.</returns>
+        private string FindLocalisedPlugin(Uri uri)
+        {
+            string filepath = null;
+
+            string localDir = LocalDirectory(uri);
+            if (!Directory.Exists(localDir))
+                return null;
+
+            // Go by convention to derive the plugin DLL's name, taking the name of the
+            // plugin directory--itself derived from the URL--and stripping any trailing
+            // version numbers that come after a dot.
+            string DllName = Path.GetFileName(localDir);
+            if (DllName.IndexOf('.') > 0)
+                DllName = DllName.Substring(0, DllName.IndexOf('.'));
+            DllName = $"{DllName}.dll";
+
+            var searchPaths = SearchTree(localDir);
+            foreach (string dir in searchPaths)
+            {
+                filepath = Path.Combine(dir, DllName);
+                if (File.Exists(filepath))
+                    break;
+            }
+
+            if (File.Exists(filepath))
+                return filepath;
+            return null;
+        }
+
+        private string LocalDirectory(Uri uri)
+        {
+            string filepath = uri.GetComponents(UriComponents.Path, UriFormat.Unescaped);
+            string filename = Path.GetFileNameWithoutExtension(filepath);
+            return Path.Combine(AHostProxy.Current.BaseDirectory, "Plugins", filename);
+        }
+
+        private IEnumerable<string> SearchTree(string root)
+        {
+            yield return root;
+
+            foreach (string dir in Directory.EnumerateDirectories(root))
+                foreach (string qdir in SearchTree(dir))
+                    yield return qdir;
+
+            yield break;
         }
     }
 }
